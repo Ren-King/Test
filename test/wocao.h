a28ef6f50add7d4c4091f58fcbeaeb6289693658
Your Name <2382488060@qq.com>
2017-10-09 15:41:25 +0800

    “提交信息”

---
+++ test/wocao.h
@@ -0,0 +1,13 @@
+//
+//  afafa.h
+//  test
+//
+//  Created by nykj-mac-03 on 2017/9/30.
+//  Copyright © 2017年 12412. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface wocao : UIView
+
+@end
---
+++ test/wocao.m
@@ -0,0 +1,21 @@
+//
+//  afafa.m
+//  test
+//
+//  Created by nykj-mac-03 on 2017/9/30.
+//  Copyright © 2017年 12412. All rights reserved.
+//
+
+#import "wocao.h"
+
+@implementation wocao
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
